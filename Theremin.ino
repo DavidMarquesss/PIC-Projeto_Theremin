1e85fee67b680338e383e97718493c6cd9b7b21e
DavidMarquesss <119422985+DavidMarquesss@users.noreply.github.com>
2023-01-10 17:21:07 -0300

    aaaa

--- Theremin.ino
+++ Theremin.ino
@@ -5,7 +5,7 @@
 #define speaker 10
 int distancia;
 
-Ultrasonic ultrasonic(12, 13);
+Ultrasonic ultrasonic(12, 13); //define os pinos 12 - Trigger e 13 - Echo para o sensor ultrassonico
 
 void setup() {
   Serial.begin(9600);
@@ -15,17 +15,17 @@ void setup() {
 void loop() 
 {
   delay(30);                    
-  distancia = ultrasonic.read(); 
+  distancia = ultrasonic.read(); //Pega a leitura de distancia em centímetros do sensor ultrassonico (limite de 120 cm)
   if(distancia < 120 )             
   {
-    int freq= 1500-distancia*10;   
+    int freq= 1500-distancia*10;  //calculo para a frequencia que sera em seguida escutada pelo speaker 
     toneAC(freq, speaker);    
     
-    Serial.println(distancia);     
+    Serial.println(distancia); //mostra a distancia no serial monitor para checagens    
   }
   else
   {
-   noToneAC();   
+   noToneAC();   //caso esteja fora do alcance nada eh tocado
   }
 
 }
